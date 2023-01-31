054e6c613439bb292af1a3d49a231c82ad2c36f2
Zhiyao <15222848787@163.com>
2023-01-31 12:55:31 -0800

    📝  add map

---
+++ dictionary/dictionary.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"testing"
+)
+
+type (
+	Dictionary    map[string]string
+	DictionaryErr string
+)
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definiton, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definiton, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+
+	case nil:
+		return ErrWordExists
+
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	//map's inner function
+	delete(d, word)
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
+	}
+}
